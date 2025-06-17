86dbe0e787492338b49bd40905c259b2602f057a
Dolisha Gandhi <dolishagandhi@gmail.com>
2025-06-17 17:12:14 +0530

    Add files via upload

---
+++ Task1A1.py
@@ -0,0 +1,18 @@
+a=int(input('enter first number'))
+b=int(input('enter second number'))
+
+#addition
+c=a+b
+print('addition',c)
+
+#subtraction
+c=a-b
+print('subtraction',c)
+
+#multiplication
+c=a*b
+print('multiplication',c)
+
+#division
+c=a//b
+print('division',c)
---
+++ Task2A1.py
@@ -0,0 +1,5 @@
+Firstn=input('enter first name')
+Lastn=input('enter last name')
+
+print("Hello ,{} {}! Welcome to the Python program ".format(Firstn,Lastn))
+
